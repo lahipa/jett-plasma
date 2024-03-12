c815aaed2c66ff3a2e40adedae8b7da95330fd2a
Yoga Lahipa <dwiyogalahipa@gmail.com>
2024-03-12 20:45:26 +0700

    feat: animation parallax prep

--- src/app/@clients/index.js
+++ src/app/@clients/index.js
@@ -5,6 +5,7 @@ import LangSelector from "./langSelector";
 import SliderVideosHome from "./sliderVideosHome";
 import BeforeAfterImage from "./beforeAfterImage";
 
+export * from "./homeSections";
 export {
     CtaHomeHero,
     CtaHeaderContact,
